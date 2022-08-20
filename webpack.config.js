e196da5797cb376191942d683c152a5b3d7beea6
RamanSaksonau <kramans1990@outlook.com>
2022-08-20 17:45:50 +0300

    fix: add WebPackCopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -39,13 +39,12 @@ const baseConfig = {
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
-        // new CopyPlugin({
-        //          patterns: [
+        new CopyPlugin({
+                 patterns: [                
+                  { from: path.resolve(__dirname, './src','assets'), to: '../dist/assets'},
                 
-        //           { from: path.resolve(__dirname, './async-race','assets'), to: '../dist/assets'},
-                
-        //          ],
-        //        }),
+                 ],
+               }),
     ],
 };
 module.exports = ({ mode }) => {
